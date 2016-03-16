cfc1584211177c9fb89092e5c941717c8528ef96
Zachary Cohn <zaccohn@gmail.com>
2016-03-15 20:20:00 -0400

    adds create to classifieds

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -1,10 +1,32 @@
 class ClassifiedsController < ApplicationController
-	def new
-	end
 
 	def index
 		@classifieds = Classified.all
 	end
+	
+	def new
+		@classified = Classified.new
+		@classified.build_category
+	end
 
+	def create
+		@classified = Classified.new(safe_classified_params)
+		@classified.build_category(safe_category_params)
 
+		if @classified.save
+			redirect_to classifieds_path
+		else
+			render 'new'
+		end
+	end
+
+private
+
+	def safe_classified_params
+		params.require(:classified).permit(:title, :price, :description)
+	end
+
+	def safe_category_params
+		params.require(:category).permit(:name)
+	end
 end
